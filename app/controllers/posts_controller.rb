60eae52d4e59d536cb71e8a5aaa7de7bf2bd5a1d
Ajith <ajithkgatty@gmail.com>
2017-09-16 15:57:44 +0530

    initial commit, post creation

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+	before_action :get_post, except: [:index, :create, :new ]
+
+	def index
+		@posts = Post.all.order('created_at DESC')
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to posts_path
+		else
+			render 'new'
+		end
+	end
+
+	def show
+	end
+
+	def update
+		if @post.update_attributes(post_params)
+			redirect_to posts_path
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		if @post.destroy
+			redirect_to posts_path
+		#else
+			#render 'edit'
+		end		
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end
+
+	def get_post
+		@post = Post.find(params[:id])
+	end
+
+
+end
