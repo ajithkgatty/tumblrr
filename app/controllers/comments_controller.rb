639b2764a06f2aad8d4c2de017bc0098a6416123
Ajith <ajithkgatty@gmail.com>
2017-09-16 23:47:32 +0530

    user restrictions and minor changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+	before_action :authenticate_user!
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(comment_params)
@@ -12,10 +13,7 @@ class CommentsController < ApplicationController
 		redirect_to post_path @post
 
 	end
-
-
-
-
+	
 	private
 	def comment_params
 		params.require(:comment).permit(:name, :body)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
 	before_action :get_post, except: [:index, :create, :new ]
 
 	def index
